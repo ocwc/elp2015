5f271b9ae0d38f8cb3d9543704a03edab2f1c0a4
Jure Cuhalev <gandalfar@gmail.com>
2015-02-25 12:15:29 +0100

    added image from design

--- home.php
+++ home.php
@@ -10,8 +10,9 @@
 
 <div class="container container-white container-white-home">
 	<div class="row entry-title-row">
-		<div class="col-sm-12">
-			<h2 class="entry-title">
+		<img class="entry-title-image" src="<?php echo get_template_directory_uri(); ?>/img/top_image.png" />
+		<div class="col-sm-7 pull-right">
+			<h2 class="entry-title text-right">
 				<a href="/" rel="bookmark">Welcome to the eLearning Pioneers Web site</a>
 			</h2>
 		</div>
--- partials/content.php
+++ partials/content.php
@@ -1,6 +1,7 @@
 <div class="container container-white">
 	<div class="row entry-title-row">
-		<div class="col-sm-12 col-md-offset-1 col-md-6">
+		<img class="entry-title-image" src="<?php echo get_template_directory_uri(); ?>/img/top_image.png" />
+		<div class="col-sm-7 pull-right">
 			<h2 class="entry-title">
 				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h2>

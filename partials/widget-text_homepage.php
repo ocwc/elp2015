cb26de1642e1a3fd3172f7e24410628dc0547647
Jure Cuhalev <gandalfar@gmail.com>
2015-04-02 15:38:19 +0200

    cleanup design a bit

--- partials/widget-text_homepage.php
+++ partials/widget-text_homepage.php
@@ -1,18 +1,30 @@
 <div class="col-sm-4">
 	<a class="highlight-box <?php echo 'highlight-box-' . get_field('widget_background_color', 'widget_' . $widget_id ); ?>" 
 		href="<?php the_field('widget_url', 'widget_' . $widget_id ); ?>">
+
+		<!--
+		<img class="" 
+			 src="<?php echo get_field('widget_image', 'widget_' . $widget_id )['url']; ?>" />
+		-->
+
+		<?php $url = get_field('widget_url', 'widget_' . $widget_id ); ?>
+		<?php if ( strpos($url, '/about') > 0) : ?>
+			<icon class="fa fa-question"></icon>
+		<?php elseif ( strpos($url, '/schedules') > 0) : ?>
+			<icon class="fa fa-pencil"></icon>
+		<?php else : ?>
+			<icon class="fa fa-desktop"></icon>
+		<?php endif; ?>
 		
 		<span class="highlight-title"><?php echo $title; ?></span>
 
-		<img class="pull-right" 
-			 src="<?php echo get_field('widget_image', 'widget_' . $widget_id )['url']; ?>" />
 	</a>
 
 	<p class="highlight-description">
 		<?php the_field('widget_description', 'widget_' . $widget_id ); ?>
 	</p>
 
-	<a class="btn <?php echo 'btn-' . get_field('widget_background_color', 'widget_' . $widget_id ); ?> btn-home-readmore" 
+	<a class="btn btn-violet <?php echo 'btn-' . get_field('widget_background_color', 'widget_' . $widget_id ); ?> btn-home-readmore" 
 		href="<?php the_field('widget_url', 'widget_' . $widget_id ); ?>">Read more</a>
 
 </div>
